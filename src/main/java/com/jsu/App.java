2adcbeb3fd0253269ff0443c81a66404e779b120
hhr <1491946045@qq.com>
2022-04-21 19:18:57 +0800

    first commit

---
+++ src/main/java/com/jsu/App.java
@@ -0,0 +1,15 @@
+package com.jsu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hello git");
+
+    }
+}
