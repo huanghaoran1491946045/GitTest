35695858af968ed2076012f1af1286bc07544dc7
hhr <1491946045@qq.com>
2022-04-21 19:39:16 +0800

    second commit

--- src/main/java/com/jsu/App.java
+++ src/main/java/com/jsu/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println("hello git!1");
         System.out.println("hello git!2");
         System.out.println("hello git!3");
+        System.out.println("master test");
 
     }
 }
