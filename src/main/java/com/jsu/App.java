18ec494004264e79bb035db6e9b177e3f4cc08d6
hhr <1491946045@qq.com>
2022-04-21 19:30:31 +0800

    hot-fix commit1

--- src/main/java/com/jsu/App.java
+++ src/main/java/com/jsu/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hello git!1");
         System.out.println("hello git!2");
+        System.out.println("hello git!3");
 
     }
 }
